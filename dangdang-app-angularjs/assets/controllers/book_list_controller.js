c9489d7937b9607bce73b5f538b8fd78c4977698
1090023566 <q1090023566.github.io>
2016-10-20 09:45:30 +0800

    小错误

--- dangdang-app-angularjs/assets/controllers/book_list_controller.js
+++ dangdang-app-angularjs/assets/controllers/book_list_controller.js
@@ -1,6 +1,6 @@
 app.controller('bookListController',['$scope','$routeParams','commonService',function($scope,$routeParams,c_service){
   $scope.data = [];
-    c_s.getData($routeParams.id,function(res){
+    c_service.getData($routeParams.id,function(res){
       // console.dir(res);
       $scope.data = res.data;
     });
